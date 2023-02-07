403106adbaf32cdd35a527f805ef1a31c35ca885
david elzinga <delzinga@vols.utk.edu>
2023-02-06 20:07:28 -0500

    2/6/23

--- tri_plot.py
+++ tri_plot.py
@@ -24,41 +24,48 @@ p['omega'] = 0.5425; p['alpha'] = 0.0833; p['tau'] = 1 - p['alpha'] - p['omega']
 p['mu'] = 0.2930; p['nu'] = 2.3774;  p['gamma'] = 1; p['beta'] = 10**(0.1)
 p['r_S'] = 0.8; p['r_P'] = 0.4; p['u'] = 0.8
 
-alphas = []; omegas = []; taus = [];
-cond_1s = []; cond_3s = [];
-for p['alpha'] in np.linspace(0, 1, 100, endpoint=False):
-    for p['omega'] in np.linspace(0, 1 - p['alpha'], 100):
-        p['tau'] = 1 - p['alpha'] - p['omega']
-        p['T'] = p['tau']*p['gamma'];
-        p['Omega'] = p['nu']*p['omega']
-        p['A'] = p['alpha']*p['beta']
+fig = plt.figure(figsize=(14.8, 4.8))
 
-        cond_1s.append(cond_1(p))
-        cond_3s.append(cond_3(p))
-        alphas.append(p['alpha']); omegas.append(p['omega']); taus.append(p['tau'])
+for k, p['beta'] in enumerate([1.1, 2, 3.5]):
+    alphas = []; omegas = []; taus = [];
+    cond_1s = []; cond_3s = [];
+    for p['alpha'] in np.linspace(0, 1, 100, endpoint=False):
+        for p['omega'] in np.linspace(0, 1 - p['alpha'], 100):
+            p['tau'] = 1 - p['alpha'] - p['omega']
+            p['T'] = p['tau']*p['gamma'];
+            p['Omega'] = p['nu']*p['omega']
+            p['A'] = p['alpha']*p['beta']
 
-vmin = 0.0
-vmax = 1
+            cond_1s.append(cond_1(p))
+            cond_3s.append(cond_3(p))
+            alphas.append(p['alpha']); omegas.append(p['omega']); taus.append(p['tau'])
 
-fig = plt.figure(figsize=(10.8, 4.8))
-ax = fig.add_subplot(1, 1, 1, projection='ternary')
+    vmin = 0.0
+    vmax = 1
 
-cond_1_cmap = matplotlib.colors.ListedColormap(['white', 'blue'])
-cs = ax.tripcolor(
-    alphas, omegas, taus, cond_1s, shading='flat', alpha = 0.5, vmin=vmin, vmax=vmax, rasterized=True, cmap=cond_1_cmap, linewidth=0.0, antialiased=True, edgecolors='face')
+    ax = fig.add_subplot(1, 3, k + 1, projection='ternary')
 
-cond_3_cmap = matplotlib.colors.ListedColormap(['white', 'red'])
-cs = ax.tripcolor(
-    alphas, omegas, taus, cond_3s, shading='flat', alpha = 0.5, vmin=vmin, vmax=vmax, rasterized=True, cmap=cond_3_cmap, linewidth=0.0, antialiased=True, edgecolors='face')
+    cond_1_cmap = matplotlib.colors.ListedColormap(['white', 'blue'])
+    cs = ax.tripcolor(
+        alphas, omegas, taus, cond_1s, shading='flat', alpha = 0.5, vmin=vmin, vmax=vmax, rasterized=True, cmap=cond_1_cmap, linewidth=0.0, antialiased=True, edgecolors='face')
 
-ax.scatter(0.0833, 0.5425, 0.3742, color='red')
+    cond_3_cmap = matplotlib.colors.ListedColormap(['white', 'red'])
+    cs = ax.tripcolor(
+        alphas, omegas, taus, cond_3s, shading='flat', alpha = 0.5, vmin=vmin, vmax=vmax, rasterized=True, cmap=cond_3_cmap, linewidth=0.0, antialiased=True, edgecolors='face')
+    
+    ax.scatter(0.0833, 0.5425, 0.3742, color='red')
 
-cax = ax.inset_axes([1.05, 0.1, 0.05, 0.9], transform=ax.transAxes)
-colorbar = fig.colorbar(cs, cax=cax)
-colorbar.set_label('Entropy', rotation=270, va='baseline')
+    ax.set_tlabel(r'$\alpha$', fontsize=15)
+    ax.set_llabel(r'$\omega$', fontsize=15)
+    ax.set_rlabel(r'$\tau$', fontsize=15)
 
-ax.set_tlabel(r'$\alpha$')
-ax.set_llabel(r'$\omega$')
-ax.set_rlabel(r'$\tau$')
+    ax.taxis.set_label_rotation_mode('horizontal')
+    ax.laxis.set_label_rotation_mode('horizontal')
+    ax.raxis.set_label_rotation_mode('horizontal')
 
+    ax.set_title(r'$\beta = $' + str(p['beta']), y=0, pad=-35, verticalalignment="top", fontsize=15)
+
+plt.subplots_adjust(wspace=0.6)
+
+fig.savefig('tri_plot.pdf', bbox_inches='tight')
 plt.show()
