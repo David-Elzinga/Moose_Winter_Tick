9431f608b35b34a85a734031bfae326bf77bb093
david elzinga <delzinga@vols.utk.edu>
2023-02-12 12:32:58 -0500

    adding hunting control

---
+++ hunting_control.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import multiprocessing
+from wildlife_management_model import simulate
+import matplotlib.pyplot as plt
+from itertools import product
+
+def worker(obj):
+
+    # Specify the initial conditions. Group them into an array 'Z'. 
+    S0 = 1000; E0 = 250; P0 = 250; H0 = 52000*250; Q0 = 0
+    Z0 = np.array([S0, E0, P0, H0, Q0])
+    Zsol = [Z0]; tsol = [0]
+
+    # Define model parameters. 
+    parm = {}
+    parm['omega'] = 0.5425; parm['alpha'] = 0.0833; parm['tau'] = 1 - parm['alpha'] - parm['omega']
+    parm['mu'] = 0.2835; parm['nu'] = 2.3772;  parm['gamma'] = 1.8523; parm['beta'] = 10**(-2)
+    parm['r_S'] = 0.8590; parm['r_P'] = 0.4998; parm['u'] = 0.75
+
+    parm['eta'] = 1.1886; parm['xi'] = 52000; parm['q'] = 0.75; parm['K'] = 1500; parm['r_T'] = 13.0293
+    parm['c'] = 10**(-2); parm['beta_T'] = parm['beta']/1; parm['beta_M'] = parm['beta']/parm['xi']
+
+    # Unpack the hunting params.
+    print(obj)
+    parm['mu_omega'] = obj[0]; parm['mu_alpha'] = obj[1]
+
+    # Run the model
+    num_years = 200
+    tsol, Zsol, Zwinter = simulate(num_years, init_cond=[S0, E0, P0, H0, Q0], p=parm, granularity=2, thresh = 10)
+
+    if len(Zwinter) == num_years + 1:
+        return (max(Zwinter[100:]), min(Zwinter[100:]))
+    else:
+        return (np.nan, np.nan)
+
+def main(pool):
+
+    # Construct a grid of values for the hunting rates. 
+    s = 100
+    mu_omega_vals =  np.linspace(0, 1, s)
+    mu_alpha_vals = np.linspace(0, 1, s)
+
+    # Construct a df to hold all parameter sets
+    df = pd.DataFrame(list(product(mu_omega_vals, mu_alpha_vals)), columns=['mu_omega', 'mu_alpha'])
+
+    # Run all time series. Extract minimum and maximum population.
+    results = pool.map(worker, df.values)
+    df['max_pop'] = [r[0] for r in results]
+    df['min_pop'] = [r[1] for r in results]
+
+    # Plot! 
+    fig, ax = plt.subplots(1, 2, figsize=(9,4), sharex=True, sharey=True)
+    CS = ax[0].contourf(df['mu_omega'].values.reshape(s,s), df['mu_alpha'].values.reshape(s,s), df['min_pop'].values.reshape(s,s), cmap='viridis')
+    ax[0].set_title('Minimum Herd Population')
+    ax[0].set_xlabel(r'$\mu_\omega$', fontsize=20); ax[0].set_ylabel(r'$\mu_\alpha$', fontsize=20)
+    cbar = fig.colorbar(CS, ax = ax[0])
+
+    CS = ax[1].contourf(df['mu_omega'].values.reshape(s,s), df['mu_alpha'].values.reshape(s,s), df['max_pop'].values.reshape(s,s), cmap='viridis')
+    ax[1].set_title('Maximum Herd Population')
+    ax[1].set_xlabel(r'$\mu_\omega$', fontsize=20); ax[1].set_ylabel(r'$\mu_\alpha$', fontsize=20)
+    cbar = fig.colorbar(CS, ax = ax[1])
+
+    plt.savefig('fig_hunting_cycles.pdf', bbox_inches='tight')
+    plt.show()
+
+
+if __name__ == '__main__':
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)
+    main(pool)
--- time_series.py
+++ time_series.py
@@ -9,14 +9,15 @@ model_type = 'wmm'
 # Define model parameters. 
 parm = {}
 parm['omega'] = 0.5425; parm['alpha'] = 0.0833; parm['tau'] = 1 - parm['alpha'] - parm['omega']
-parm['mu'] = 0.2835; parm['nu'] = 2.3772;  parm['gamma'] = 1.8523; parm['beta'] = 10**(0.5)
+parm['mu'] = 0.2835; parm['nu'] = 2.3772;  parm['gamma'] = 1.8523; parm['beta'] = 10**(-2)
 parm['r_S'] = 0.8590; parm['r_P'] = 0.4998; parm['u'] = 0.75
 
-parm['eta'] = 1.1886; parm['xi'] = 52000; parm['q'] = 0.9; parm['K'] = 1500; parm['r_T'] = 8#13.0293
+parm['eta'] = 1.1886; parm['xi'] = 52000; parm['q'] = 0.75; parm['K'] = 1500; parm['r_T'] = 13.0293
 parm['c'] = 10**(-2); parm['beta_T'] = parm['beta']/1; parm['beta_M'] = parm['beta']/parm['xi']
+
 parm['mu_alpha'] = 0; parm['mu_omega'] = 0
 
-parm = {'mu_alpha': 0, 'mu_omega': 0, 'omega': 0.520665625, 'alpha': 0.048621875, 'tau': 0.43071249999999994, 'mu': 0.26825, 'nu': 2.9766515625, 'gamma': 2.0028156249999998, 'beta': 89.76871324473142, 'r_S': 0.9395312499999999, 'r_P': 0.471675, 'u': 0.28515625, 'eta': 1.121746875, 'xi': 58650.0, 'q': 0.890625, 'K': 1509.375, 'r_T': 4.7725953125, 'c': 2053.525026457146, 'beta_T': 89.76871324473142, 'beta_M': 0.001530583346031226, 'n': 200, 'H+/P+': 65107.43399583003, 'H+/S+': 13834.432872996773, 'H+/E+': 334904.52900515794, 'b': 0.9603301197018808, 'P-/H-': 0.0}
+#parm = {'mu_alpha': 0, 'mu_omega': 0, 'omega': 0.520665625, 'alpha': 0.048621875, 'tau': 0.43071249999999994, 'mu': 0.26825, 'nu': 2.9766515625, 'gamma': 2.0028156249999998, 'beta': 89.76871324473142, 'r_S': 0.9395312499999999, 'r_P': 0.471675, 'u': 0.28515625, 'eta': 1.121746875, 'xi': 58650.0, 'q': 0.890625, 'K': 1509.375, 'r_T': 4.7725953125, 'c': 2053.525026457146, 'beta_T': 89.76871324473142, 'beta_M': 0.001530583346031226, 'n': 200, 'H+/P+': 65107.43399583003, 'H+/S+': 13834.432872996773, 'H+/E+': 334904.52900515794, 'b': 0.9603301197018808, 'P-/H-': 0.0}
 #print((1/parm['beta']) * (np.log(1 + parm['r_S'] - ((parm['r_P'] + 1 - np.exp(-parm['gamma']*parm['tau'])*(1 + parm['u']*parm['r_P']))*(np.exp(parm['mu']) - parm['r_S'] - 1))/(np.exp(parm['mu'] + parm['nu']*parm['omega']) - parm['r_P'] - 1)) - parm['mu']))
 
 num_years = 200
@@ -58,6 +59,7 @@ else:
 
     # Run the model numerically
     t0 = time.time()
+    print(parm)
     tsol, Zsol, Zwinter_ODEs = wmm.simulate(num_years, init_cond=Z0, p=parm, granularity=2, thresh=10)
     t1 = time.time()
     print(t1- t0)
