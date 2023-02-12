2ebbcc2de3e69015f38027585c2a22937a8ab402
david elzinga <delzinga@vols.utk.edu>
2023-02-12 13:23:09 -0500

    needing hermione

--- oscillations.py
+++ oscillations.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 from itertools import product
 from scipy.optimize import minimize
-from tri_seasonal_model import simulate
+from wildlife_management_model import simulate
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--ncores", type=int, help="number of cores", default=os.cpu_count() - 2)
@@ -17,36 +17,39 @@ parser.add_argument("-s", "--samplesize", type=int, help="sample sizes per param
 
 def worker(param_values):
 
+    # Unpack the parameters that change
     parm = {}
-    # These parameters do not vary. 
-    parm['alpha'] = 0.2532; parm['omega'] = 0.2819; parm['tau'] = 0.4649
-    parm['mu_omega'] = 0; parm['mu_tau'] = 0; parm['mu_alpha'] = 0
-    parm['K'] = 1500 
-    parm['mu'] = 0.293; parm['eta'] = 1.3352; parm['nu'] = 2.3774/23337.7315
-    parm['d'] = 10**(-5)
-    parm['gamma'] = 1.0755; parm['r'] = 0.8572
-    parm['xi'] = 23337.7315
-
     parm['beta'] = 10**param_values[0]
     parm['r_T'] = param_values[1]
     parm['q'] = param_values[2]
 
+    # These parameters do not vary. 
+    parm['omega'] = 0.5425; parm['alpha'] = 0.0833; parm['tau'] = 1 - parm['alpha'] - parm['omega']
+    parm['mu'] = 0.2835; parm['nu'] = 2.3772;  parm['gamma'] = 1.8523
+    parm['r_S'] = 0.8590; parm['r_P'] = 0.4998; parm['u'] = 0.75
+
+    parm['eta'] = 1.1886; parm['xi'] = 52000; parm['K'] = 1500
+    parm['c'] = 10**(-2); parm['beta_T'] = parm['beta']/1; parm['beta_M'] = parm['beta']/parm['xi']
+
+    parm['mu_alpha'] = 0; parm['mu_omega'] = 0
+
     # Run the model
     num_years = 200
-    S0 = 800; E0 = 50; I0 = 120; TH0 = 10**4; TQ0 = 0
-    tsol, Zsol, Zwinter = simulate(num_years, init_cond=[S0, E0, I0, TH0, TQ0], parm=parm, granularity=3, thresh=10)
+    S0 = 1000; E0 = 250; P0 = 250; H0 = 52000*250; Q0 = 0
+    tsol, Zsol, Zwinter = simulate(num_years, init_cond=[S0, E0, P0, H0, Q0], p=parm, granularity=2, thresh=10)
 
     if len(Zwinter) < num_years: # In this case there was an extinction
         return (np.nan, np.nan, np.nan, np.nan) # std dev, a, b, c (fits)
     else: # Not an extinction, record an estimate of the parameters to fit the cycles
         
         def func(x_data, a, b, c):
-            return a*np.cos(b*x_data) + c
+            return a*np.cos(b*x_data) + c # general cosine function
 
         def cost(x):
             a = x[0]; b = x[1]; c = x[2]
-            return sum((y_data - func(x_data, a, b, c))**2)
+            return sum((y_data - func(x_data, a, b, c))**2) # sum of squared residuals between this cosine and the real data
 
+        # Define various initial conditions for the cosine to avoid getting trapped in local minima
         a_guesses = np.linspace(0, 1500, 5)
         b_guesses = np.linspace(np.pi/50, np.pi, 5)
         c_guesses = np.linspace(0, 1000, 5)
@@ -57,12 +60,14 @@ def worker(param_values):
         guesses = list(product(a_guesses, b_guesses, c_guesses))
         cost_of_guesses = []
         fits = []
-
+        
+        # For each initial condition, search for the minimum value witin certain bounds
         for guess in guesses:
             fit = minimize(cost, guess, method='Nelder-Mead', bounds=((None, None), (0, np.pi), (None, None)), tol=1e-6)
             fits.append(fit)
             cost_of_guesses.append(fit.fun)
 
+        # Consider the lowest cost fit to be the global minimum
         best_fit_indx = cost_of_guesses.index(min(cost_of_guesses))
         popt = fits[best_fit_indx].x
         return (np.std(Zwinter[-100:]), popt[0], popt[1], popt[2])
@@ -70,9 +75,9 @@ def worker(param_values):
 def main(pool, s):
 
     # Define the number of samples we want for each parameter within its range. 
-    logbeta_vals =  np.linspace(-4, 0, s)
+    logbeta_vals =  np.linspace(-2, 1, s)
     r_T_vals = np.linspace(3.2588, 35.5531, s)
-    q_vals = np.array([0.1, 0.5, 0.9])
+    q_vals = np.array([0.75, 0.9])
 
     # Construct a df to hold all parameter sets
     df = pd.DataFrame(list(product(logbeta_vals, r_T_vals, q_vals)), columns=['logbeta', 'r_T', 'q_vals'])
@@ -97,7 +102,7 @@ def plot():
 
     import pdb; pdb.set_trace()
     # Plot the results! 
-    fig, axes = plt.subplots(1, 3, figsize=(9,4), sharex=True, sharey=True)
+    fig, axes = plt.subplots(1, 2, figsize=(9,4), sharex=True, sharey=True)
     for ax, q_val in zip(axes, df['q_vals'].unique()):
         sub_df = df[df['q_vals'] == q_val]
         CS = ax.contourf(sub_df['logbeta'].values.reshape(s,s), sub_df['r_T'].values.reshape(s,s), sub_df['std'].values.reshape(s,s), cmap='viridis')
@@ -116,7 +121,7 @@ def plot():
     df.loc[df['std'] < 100 ,'b'] = np.nan
     df['cycle_length'] = 2*np.pi / np.abs(df['b'])
     df.loc[df['cycle_length'] > 100, 'cycle_length'] = np.nan
-    fig, axes = plt.subplots(1, 3, figsize=(9,4), sharex=True, sharey=True)
+    fig, axes = plt.subplots(1, 2, figsize=(9,4), sharex=True, sharey=True)
     for ax, q_val in zip(axes, df['q_vals'].unique()):
         sub_df = df[df['q_vals'] == q_val]
         CS = ax.contourf(sub_df['logbeta'].values.reshape(s,s), sub_df['r_T'].values.reshape(s,s), sub_df['cycle_length'].values.reshape(s,s), cmap='viridis')
